7dee471227ce1a75b1947cfa36548e42fccc815d
danilatang <d6nil9@gmail.com>
2025-02-13 10:41:06 +0300

    feature/#1-changelogs created

---
+++ src/main/java/com/example/restappuserstable/RestAppUsersTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.restappuserstable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppUsersTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAppUsersTableApplication.class, args);
+    }
+
+}
